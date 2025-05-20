d7469e4331c52b060c486b758ed98a16366bd5f1
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-20 21:03:52 +0500

    Initial commit

---
+++ ap.js
@@ -0,0 +1,141 @@
+// array basic practice exercise
+// ========= array-practice small project-01(store 3 favourite colors)========= 
+    // var favouriteColors = ['red','green','blue'];
+    // alert(favouriteColors[0]);
+    // alert(favouriteColors[1]);
+    // alert(favouriteColors[2]);
+
+    // ========= array-practice small project-02(prompt)========= 
+            // var item1 = prompt('Enter atom one name');
+            // var item2 = prompt('Enter atom 2 name');
+            // var array = item1;
+            // var array2 = item2;
+            // var newarray = [array,array2];
+            // alert(newarray);
+
+
+            // ========= array-practice small project-03()========= 
+                    // var cities = ['islamabad','rawalpindi','attock','mianwali','lahore'];
+                    // alert('The first city is ' + cities[0]);
+                    // alert('The second city is ' + cities[1]);
+                    // alert('The third city is ' + cities[2]);
+                    // alert('The fourth city is ' + cities[3]);
+                    // alert('The fifth city is ' + cities[4]);
+
+
+            // ========= array-practice small project-04()========= 
+                            // var friends = ['ali','huzaifa','amir'];
+                            // alert(friends[0]);
+                            // alert(friends[1]);
+                            // alert(friends[2]);
+
+
+                            // ========= array-practice small project-05()========= 
+                                    // var dataRecord = [24,'smit','karachi'];
+                                    // alert('My age is : ' + dataRecord[0]); 
+                                    // alert('My school is ' + dataRecord[1]);
+                                    // alert('My city is ' + dataRecord[2]);
+
+
+                                    // ========= array-practice small project-06()========= 
+
+                                        // var subjects = ['math','englis','science'];
+                                        // alert('I like ' + subjects[0]+' ' + subjects[1]+ ' and ' + subjects[2]);
+                                        // ========= array-practice small project-07()========= 
+                                                // var sentence = ['i','am','learnig','web','development'];
+                                                // alert(sentence[0] + ' ' + sentence[1]+' ' + sentence[2]+ ' ' + sentence [3]+' ' + sentence[4] + '.');
+
+
+                                                // ========= array-practice small project-08()========= 
+                                                        // var marks = [54,89,67];
+                                                        // var student1Marks = marks[0];
+                                                        // var student2Marks = marks[1];
+                                                        // var student3Marks = marks[2];
+                                                        // var total = student1Marks + student2Marks + student3Marks;
+                                                        // alert('The three students got total ' + total + ' marks.'); 
+
+
+                                                        // ========= array-practice small project-09()========= 
+                                                        //  var persons = ['ahmed','hadi', 'saqib'];
+                                                        //  alert('I say welcome to ' + persons[0] + ' in smit.');
+                                                        //  alert('I say welcome  to ' + persons[1] + ' in smit.');
+                                                        //  alert('I say welcome  to ' + persons[2] + ' in smit.');
+                                                        
+
+                                                        // ========= array-practice small project-10()========= 
+                                                        // var array = [2000,500];
+                                                        // var number1 = array[0];
+                                                        // var number2 = array[1];
+                                                        // var addition = number1 + number2;
+                                                        // alert('the addition of ' + number1 + ' and ' + number2 + ' is ' + addition);
+
+                                                        
+                                                        // ========= array-practice small project-11()========= 
+                                                                // var firstName = prompt('Enter your first name');
+                                                                // var secondName = prompt('Enter your second name');
+                                                                // var array1 = firstName;
+                                                                // var array2 = secondName;
+                                                                // var newArray  = [array1,array2];
+                                                                // var fullName = array1 + array2;
+                                                                // alert('My full name is ' + fullName + '.');
+
+                                                                // ========= array-practice small project-12()=========
+                                                                
+                                                                        // var mathMarks = +prompt('Enter math obtained marks out of 100');
+                                                                        // var englishMarks = +prompt('Enter english obtained marks out of 100');
+                                                                        // var scieneMarks = +prompt('Enter science obtained marks out of 100');
+                                                                        // var urduMarks = +prompt('Enter urdu obtained marks out of 100');
+                                                                        // var marks1 = mathMarks;
+                                                                        // var marks2 = englishMarks;
+                                                                        // var marks3 = scieneMarks;
+                                                                        // var marks4 = urduMarks;
+                                                                        // var array = [marks1,marks2,marks3,marks4];
+                                                                        // console.log(array);
+                                                                        // var totalMarks = 400;
+                                                                        // var totalObtainedMarks = array[0] + array[1] + array[2];
+                                                                        // var percentage = (totalObtainedMarks / totalMarks) * 100;
+                                                                        // alert('You got ' + percentage + '% marks in exam');
+                                                                
+// ========= array-practice small project-13()=========
+// var name = prompt('enter your name');
+// var age = prompt('enter your age');
+// var city = prompt('enter your city name');
+// var inform1 = name;
+// var inform2 = age;
+// var inform3    = city;
+// var array = [inform1,inform2,inform3];
+// console.log(array);
+// alert('My name :' + inform1 + '\n' + 'My age :' + inform2 + '\n' + 'my city is : ' + inform3 )
+                                          
+// ========= array-practice small project-14()=========
+                // var priceItem1 = +prompt('Enter item one price');
+                // var priceItem2 = +prompt('Enter item 2 price');
+                // var arr1 = priceItem1;
+                // var arr2 = priceItem2;
+                // var array = [arr1,arr1];
+                // console.log(array);
+                // var total = arr1 + arr2;
+                // alert('The total price of item one and item two is :' + total + ' PKR');
+                // var discount = (total * 10)/100;
+                // alert('The discount amount is :' + discount + ' PKR');
+                // var afterDiscount = total - discount;
+                // alert('After discount total price of two items is : ' + afterDiscount + ' PKR');
+
+                // ========= array-practice small project-15()=========
+                        // var name = prompt('enter your name');
+                        // var password = prompt('enter your password');
+                        // var arrName = name;
+                        // var arrPass = password;
+                        // var array  = [arrName,arrPass];
+                        // alert('Login successful with the entered data');
+
+                           // ========= array-practice small project-16()=========
+                        //    var name = prompt('enter your name');
+                        //    var birthYear = prompt('enter your birth year YYYY');
+                        //    var nameArray = name ;
+                        // var birtArray = birthYear;
+                        // var array = [nameArray,birtArray];
+                        // console.log(array);
+                        // var currentYear = 2025;
+                        // var newage = currentYear - array[1];
+                        // alert('your age is :' + newage + ' years');
